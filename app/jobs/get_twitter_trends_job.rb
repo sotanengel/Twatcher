df7e133c1d6eb8c5d3031a4e7ef5f68a5eee9e45
sota.n <sota.n.eng@gmail.com>
2021-05-30 15:27:46 +0900

    add twitter library and job file

---
+++ app/jobs/get_twitter_trends_job.rb
@@ -0,0 +1,7 @@
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET_KEY"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_SECRET_TOKEN"]
+end
+@trends = client.trends(id = 23424856).attrs[:trends].first(10)
---
+++ test/jobs/get_twitter_trends_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GetTwitterTrendsJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
