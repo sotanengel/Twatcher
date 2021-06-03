509e26e1c22ec35922b20df316c04c9e18be762c
sota.n <sota.n.eng@gmail.com>
2021-06-03 22:57:56 +0900

    Get Twitter Trends

--- app/jobs/get_twitter_trends_job.rb
+++ app/jobs/get_twitter_trends_job.rb
@@ -1,7 +1,12 @@
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_API_KEY"]
-  config.consumer_secret     = ENV["TWITTER_API_SECRET_KEY"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_SECRET_TOKEN"]
+class GetTwitterTrendsJob < ApplicationJob
+  queue_as :default
+  def get()
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_API_KEY"]
+      config.consumer_secret     = ENV["TWITTER_API_SECRET_KEY"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET_TOKEN"]
+    end
+    @trends = client.trends(id = 23424856).attrs[:trends].first(10)
+  end
 end
-@trends = client.trends(id = 23424856).attrs[:trends].first(10)
